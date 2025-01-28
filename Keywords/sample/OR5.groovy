bf88a40848dcb9b567b150a350b236f15c603313
hari.singh83 <hari.singh@storable.com>
2025-01-28 14:49:36 -0500

    my changes4

---
+++ Keywords/sample/OR5.groovy
@@ -0,0 +1,50 @@
+package sample
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class OR5 {
+	
+	@Keyword
+	def Claim() {
+		WebUI.openBrowser('')
+		
+		WebUI.navigateToUrl('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+		
+		WebUI.setText(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/input_Username_username'), 'admin')
+		
+		WebUI.setEncryptedText(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/input_Password_password'), 'hUKwJTbofgPU9eVlw/CnDQ==')
+		
+		WebUI.click(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/button_Login'))
+		
+		WebUI.verifyElementPresent(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/a_Claim'), 0)
+		
+		WebUI.verifyElementClickable(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/a_Claim'))
+		
+		WebUI.click(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/a_Claim'))
+		
+		WebUI.verifyElementPresent(findTestObject('Object Repository/OrangeHRM/PIM/Page_OrangeHRM/button_Assign Claim'), 0)
+		
+	}
+	@Keyword
+	def TestPass() {
+		println ('Test is Pass')
+	}
+}
